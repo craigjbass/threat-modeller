e59c9adc6b648c764f391e196bf14c251c0513dc
Craig Bass <craig@madetech.com>
2026-09-07 18:58:14 +0100

    Xcode blank project

---
+++ threatmodellerTests/threatmodellerTests.swift
@@ -0,0 +1,19 @@
+//
+//  threatmodellerTests.swift
+//  threatmodellerTests
+//
+//  Created by Craig J. Bass on 07/09/2026.
+//
+
+import Testing
+@testable import threatmodeller
+
+struct threatmodellerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
